46f7dccdf4b057201a6053e844bac759772f70a8
yanniskonstas <konstas@protonmail.com>
2020-01-26 15:27:24 +0000

    First commit

---
+++ src/main/java/konstas/micros/orders/exceptions/OrderNotFoundException.java
@@ -0,0 +1,11 @@
+package konstas.micros.orders.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Order not found")
+public class OrderNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+}
